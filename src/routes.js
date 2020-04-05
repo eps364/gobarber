c13b993a8926b0366da28c52bf30b9ee9c5f24a2
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2020-04-05 13:48:30 -0300

    Agendamentos

--- src/routes.js
+++ src/routes.js
@@ -5,21 +5,21 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import ProviderController from './app/controllers/ProviderController';
 import FileController from './app/controllers/FileController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
-routes.get('/', (request, response) =>
-  response.json({ message: 'Ola, Aplicação Rodando!' })
-);
+routes.get('/', (request, response) => {
+  return response.json({ message: 'Ola, Aplicação Rodando!' });
+});
 routes.post('/sessions', SessionController.store);
-
 routes.use(authMiddleware);
 
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
-
 routes.get('/users/:id', UserController.show);
 routes.put('/users', UserController.update);
 routes.delete('/users/:id', UserController.delete);
@@ -27,5 +27,10 @@ routes.delete('/users/:id', UserController.delete);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 export default routes;
