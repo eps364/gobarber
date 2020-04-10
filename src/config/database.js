a31aa742b8c5419c288b6479cefd07a2bc1f6437
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2020-04-10 14:17:25 -0300

    Ajustes Finais

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   dialect: process.env.DB_DIALECT,
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || DATABASE_URL,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
